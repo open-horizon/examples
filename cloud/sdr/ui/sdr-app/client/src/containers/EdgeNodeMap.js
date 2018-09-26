5c2ee488e1a0d0da3f7e78f6abbf9bf92fc43d25
Varatep Buranintu <vburani@us.ibm.com>
2018-09-25 23:25:30 -0700

    render edge node map with geolocator

--- cloud/sdr/ui/sdr-app/client/src/containers/EdgeNodeMap.js
+++ cloud/sdr/ui/sdr-app/client/src/containers/EdgeNodeMap.js
@@ -1,24 +1,44 @@
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 import React, {Component} from 'react'
 import ReactMapGL from 'react-map-gl'
+import {geolocated} from 'react-geolocated'
+
+import {
+  MAPBOX_TOKEN,
+} from '../config/settings'
 
 class EdgeNodeMap extends Component {
   state = {
     viewport: {
-      height: 400,
-      latitude: 0,
-      longitude: 0,
+      height: 800,
+      width: 800,
+      latitude: (this.props.isGeolocationAvailable && this.props.isGeolocationEnabled && this.props.coords && this.props.coords.latitude) || 41.1264849,
+      longitude: (this.props.isGeolocationAvailable && this.props.isGeolocationEnabled && this.props.coords && this.props.coords.longitude) || -73.7140195,
       zoom: 8,
     }
   }
 
   render() {
     return (
-      <ReactMapGL
-        {...this.state.viewport}
-        onViewportChange={(viewport) => this.setState({viewport})}
-      />
+      <div className="bx--row">
+        <div className="bx--col-xs-12">
+          <div>
+            <ReactMapGL
+              {...this.state.viewport}
+              onViewportChange={(viewport) => this.setState({viewport})}
+              mapboxApiAccessToken={MAPBOX_TOKEN}
+            />
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
-export default EdgeNodeMap
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(EdgeNodeMap)
