9222a0ae7dc4999db95ddabf6c718bf74006e3a5
Varatep Buranintu <vburani@us.ibm.com>
2018-09-25 23:25:42 -0700

    render Sentiment table into the proper container

--- cloud/sdr/ui/sdr-app/client/src/containers/GlobalServices.js
+++ cloud/sdr/ui/sdr-app/client/src/containers/GlobalServices.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react'
 
+import Sentiment from '../Sentiment'
+
+import './GlobalServices.css'
+
 class GlobalServices extends Component {
   render() {
     return (
       <div>
-
+      <div className="bx--row">
+        <div className="bx--col-xs-12">
+          <h1 className="page-title">Global Keyword Sentiments</h1>
+        </div>
+      </div>
+      <div className="bx--row">
+        <div className="bx--col-xs-12">
+          <p className="page-description">The top 20 keywords mentioned on all of the edge nodes.</p>
+        </div>
+      </div>
+      <br />
+      <div className="bx--row">
+        <div className="bx--col-xs-12">
+          <Sentiment />
+        </div>
+      </div>
       </div>
     )
   }
