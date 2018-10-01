995f692954038b5fc3b3060f99db2ec18445f598
Varatep Buranintu <varatep@buranintu.com>
2018-10-01 11:38:14 -0700

    style mapbox

--- cloud/sdr/ui/sdr-app/client/src/containers/EdgeNodeMap.js
+++ cloud/sdr/ui/sdr-app/client/src/containers/EdgeNodeMap.js
@@ -1,8 +1,17 @@
 import 'mapbox-gl/dist/mapbox-gl.css'
+import './EdgeNodeMap.css'
 
 import React, {Component} from 'react'
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, {
+  Marker,
+  Popup,
+  NavigationControl,
+} from 'react-map-gl'
 import {geolocated} from 'react-geolocated'
+import ReactSVG from 'react-svg'
+
+import MapMarker from '../components/MapMarker'
+import MapMarkerPopup from '../components/MapMarkerPopup'
 
 import {
   MAPBOX_TOKEN,
@@ -19,16 +28,53 @@ class EdgeNodeMap extends Component {
     }
   }
 
+  _renderCityMarker = (city, index) => {
+    return (
+      <Marker key={`marker-${index}`}
+        longitude={city.longitude}
+        latitude={city.latitude}
+      >
+        <MapMarker size={20} onClick={() => this.setState({popupInfo: city})} />
+      </Marker>
+    )
+  }
+
+  _renderPopup() {
+    const {popupInfo} = this.state
+
+    return popupInfo && (
+      <Popup tipSize={5}
+        anchor="top"
+        longitude={popupInfo.longitude}
+        latitude={popupInfo.latitude}
+        onClose={() => this.setState({popupInfo: null})} >
+        <MapMarkerPopup info={popupInfo} />
+      </Popup>
+    );
+  }
+
   render() {
+
+    const cities = [{
+      latitude: 41.1264849,
+      longitude: -73.7140195,
+    }]
+
     return (
       <div className="bx--row">
         <div className="bx--col-xs-12">
           <div>
             <ReactMapGL
+              mapStyle='mapbox://styles/mapbox/dark-v9'
               {...this.state.viewport}
               onViewportChange={(viewport) => this.setState({viewport})}
               mapboxApiAccessToken={MAPBOX_TOKEN}
-            />
+              className="edge-node-map"
+            >
+              {cities.map(this._renderCityMarker)}
+
+              {this._renderPopup()}
+            </ReactMapGL>
           </div>
         </div>
       </div>
