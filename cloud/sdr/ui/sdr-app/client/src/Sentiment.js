761e47c42786acf24057c0f79f206f60f17fd4c3
Varatep Buranintu <varatep@buranintu.com>
2018-10-02 05:05:15 -0700

    import Query component

--- cloud/sdr/ui/sdr-app/client/src/Sentiment.js
+++ cloud/sdr/ui/sdr-app/client/src/Sentiment.js
@@ -2,7 +2,10 @@
 
 // import React, { Component } from 'react';
 import React from 'react';
-import { graphql } from 'react-apollo';
+import {
+    Query,
+    graphql,
+} from 'react-apollo';
 import { gql } from 'apollo-boost';
 
 import {
