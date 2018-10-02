9e436ac01abcf966bb1d51a0f91708252ca92aba
Varatep Buranintu <varatep@buranintu.com>
2018-10-01 17:01:58 -0700

    show list of edge nodes in pop up if more than 1 node for lat/lng combo

--- cloud/sdr/ui/sdr-app/client/src/components/MapMarkerPopup.js
+++ cloud/sdr/ui/sdr-app/client/src/components/MapMarkerPopup.js
@@ -1,4 +1,5 @@
 import React, {PureComponent} from 'react'
+import moment from 'moment'
 
 export default class MapMarkerPopup extends PureComponent {
 
@@ -6,9 +7,33 @@ export default class MapMarkerPopup extends PureComponent {
     const {info} = this.props
     const displayName = `${info.city}, ${info.state}`
 
+    let renderList = undefined
+
+    if (info.length > 1) {
+      renderList = <div>
+        <h1>Multiple Nodes Found</h1>
+        <div>{info.map(o => <div key={o.edgenode}><a href={`/${o.edgenode}`}>{o.edgenode}&n</a><br /></div>)}</div>  
+      </div>
+    }
+
     return (
       <div>
-        test
+        {info.length > 1 &&
+          renderList
+        }
+        {info.length === 1 &&
+          <div>
+            <h1>{info[0].edgenode}</h1>
+            <p>
+              Latitude: {info[0].latitude}
+              <br />
+              Longitiude: {info[0].longitude}
+              <br />
+              Last Updated: {moment(info[0].timeupdated).toString()}
+            </p>
+          </div>
+        }
+        
       </div>
     )
   }
